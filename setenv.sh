f4bdd354be15be6227ac4aea2d0f2369717cae86
alexvahh <alexander.valentin@outlook.com>
2018-11-23 16:22:44 +0100

    no message

--- setenv.sh
+++ setenv.sh
@@ -1,3 +1,3 @@
 # You can pass some parameters to java here if you wish to
-JAVA_OPTS="-Xms512m -Xmx20480m -XX:PermSize=256m -XX:MaxPermSize=4096m -DJENKINS_HOME=/data/jenkins_work"
-
+JAVA_OPTS="-DJENKINS_HOME=/data/jenkins_work"
+# -Xms512m -Xmx20480m -XX:PermSize=256m -XX:MaxPermSize=4096m 
